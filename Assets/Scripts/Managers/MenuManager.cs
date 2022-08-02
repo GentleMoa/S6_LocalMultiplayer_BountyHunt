68e9604505a3d7b61d8cab55c4263b028417dc78
FelixVenne <Felix.nachtigaller@web.de>
2022-08-02 14:36:18 +0200

    Photon Pun 2 Networking Server, Lobby and Room Implementation

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,36 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    void Awake()
-    {
-        GameManager.OnGameStateChanged += LoadScene_1;
-
-        DontDestroyOnLoad(this.gameObject);
-    }
 
     public void StartGame()
     {
-        //Update the GameState
-        GameManager.Instance.UpdateGameState(GameState.MainMenu);
-    }
-
-    private void LoadScene_1(GameState state)
-    {
-        if (state == GameState.MainMenu)
-        {
-            SceneManager.LoadScene("Scene_1");
-
-            //Update the GameState
-            GameManager.Instance.UpdateGameState(GameState.FindPlayArea);
-        }
-    }
-
-    private void OnDisable()
-    {
-        GameManager.OnGameStateChanged -= LoadScene_1;
+        //Establish a connection to the server
+        NetworkManager.Instance.StartConnectToServer();
     }
 }
---
+++ Assets/Scripts/Networking/Photon PUN 2/NetworkManager.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public sealed class NetworkManager : MonoBehaviourPunCallbacks
+{
+    //Singleton pattern
+    public static NetworkManager Instance { set; get; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+
+        GameManager.OnGameStateChanged += LoadScene_1;
+    }
+
+    public void StartConnectToServer()
+    {
+        Debug.Log("Try to connect to the server...");
+
+        //Connect to server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to the server!");
+
+        //Next up join a lobby
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Ready to join multiplayer session!");
+
+        //Joining a room
+        FindMatch();
+    }
+
+    private void FindMatch()
+    {
+        Debug.Log("Find a room ...");
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        CreateNewRoom();
+    }
+
+    private void CreateNewRoom()
+    {
+        int randomRoomNum = Random.Range(0, 9999);
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = 5,
+            PublishUserId = true
+        };
+
+        PhotonNetwork.CreateRoom("Contract #" + randomRoomNum, roomOptions);
+
+        Debug.Log("Contract #" + randomRoomNum + " created!");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joining room!");
+
+        //Update the GameState
+        GameManager.Instance.UpdateGameState(GameState.MainMenu);
+    }
+
+    private void LoadScene_1(GameState state)
+    {
+        if (state == GameState.MainMenu)
+        {
+            PhotonNetwork.LoadLevel("Scene_1");
+
+            //Update the GameState
+            GameManager.Instance.UpdateGameState(GameState.FindPlayArea);
+        }
+    }
+
+    ////No need to unsubscribe from the event, since this is a singleton, which won't be destroyed
+    //private void OnDisable()
+    //{
+    //    GameManager.OnGameStateChanged -= LoadScene_1;
+    //}
+}
